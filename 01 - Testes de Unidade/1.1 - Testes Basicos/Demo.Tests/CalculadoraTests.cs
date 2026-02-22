9e802c6b042fbaddc4f5f69708f8d04560ec5895
ReinanGuilherme3 <r.guilhermedeoliveira3@gmail.com>
2026-02-22 13:52:46 -0300

    O básico sobre testes de unidade

---
+++ 01 - Testes de Unidade/1.1 - Testes Basicos/Demo.Tests/CalculadoraTests.cs
@@ -0,0 +1,31 @@
+﻿namespace Demo.Tests;
+
+public class CalculadoraTests
+{
+    [Fact]
+    public void Somar_DeveRetornarSomaDosNumeros()
+    {
+        // Arrange
+        var calculadora = new Calculadora();
+        double a = 5;
+        double b = 3;
+        // Act
+        double resultado = calculadora.Somar(a, b);
+        // Assert
+        Assert.Equal(8, resultado);
+    }
+
+    [Theory]
+    [InlineData(2, 3, 5)]
+    [InlineData(-1, 1, 0)]
+    [InlineData(10, 0, 10)]
+    public void Somar_DeveRetornarValoresSomaCorretos(int a, int b, int resultadoEsperado)
+    {
+        // Arrange
+        var calculadora = new Calculadora();
+        // Act
+        double resultado = calculadora.Somar(a, b);
+        // Assert
+        Assert.Equal(resultadoEsperado, resultado);
+    }
+}
---
+++ 01 - Testes de Unidade/1.1 - Testes Basicos/Demo/Calculadora.cs
@@ -0,0 +1,10 @@
+﻿namespace Demo;
+
+public class Calculadora
+{
+    public double Somar(double a, double b)
+     => a + b;
+
+    public int Dividir(int a, int b)
+        => a / b;
+}
